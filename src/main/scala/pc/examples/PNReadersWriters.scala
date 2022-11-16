b33f1602cb4656a4c422c662b8aa9f2982a15819
AnnaVitali <anna.vitali4@studio.unibo.it>
2022-11-16 10:56:09 +0100

    almost finish exercise 1

--- src/main/scala/pc/examples/PNReadersWriters.scala
+++ src/main/scala/pc/examples/PNReadersWriters.scala
@@ -1,5 +1,29 @@
 package pc.examples
 
-class PNReadersWriters {
+export pc.modelling.PetriNet
+import pc.utils.MSet
 
-}
+object PNReadersWriters:
+
+  enum Place:
+    case P1, P2, P3, P4, P5, P6, P7
+
+  export Place.*
+  export pc.modelling.PetriNet.*
+  export pc.modelling.SystemAnalysis.*
+  export pc.utils.MSet
+
+  def pnRW = PetriNet[Place](
+    MSet(P1) ~~> MSet(P2),
+    MSet(P2) ~~> MSet(P3, P4),
+    MSet(P3) ~~> MSet(P6, P5),
+    MSet(P4) ~~> MSet(P7) ^^^ MSet(P6),
+    MSet(P5) ~~> MSet(P5, P6),
+    MSet(P5) ~~> MSet(P7) ^^^ MSet(P6),
+    MSet(P6) ~~> MSet(P1),
+    MSet(P7) ~~> MSet(P1, P5),
+  ).toSystem
+
+@main def mainPNReadersWriters =
+  import PNReadersWriters.*
+  println(pnRW.paths(MSet(P5, P1, P1, P1), 3).toList.mkString("\n"))
